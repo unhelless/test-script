b22fd3426273c920a53ad60c2ffd78b11af26fb3
unhelless <49686073+unhelless@users.noreply.github.com>
2021-01-08 17:41:06 +1000

    Update script(new).php

--- script(new).php
+++ script(new).php
@@ -1,13 +1,17 @@
-
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/classes/general/csv_data.php"); 
 
-$BLOCK_ID = 15;
+$BLOCK_ID    = 15;
+$STEP        = 2; //количество шагов
+$count_csv   = 20; //количество строк в csv файле
+$LIMIT       = intdiv($count_csv,$STEP); 
 
 
 $filePath = $_SERVER["DOCUMENT_ROOT"]."/test.csv"; 
 $csvFile = new CCSVData('R', false);
 $csvFile->LoadFile($filePath);
 $csvFile->SetDelimiter(';'); 
+$csvFile->SetFirstHeader(true);
+
 
 CModule::IncludeModule("iblock"); 
 
@@ -28,45 +32,55 @@ $arFilter = Array(
 		"ACTIVE"=>"Y"); 
 
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+
 $ress= array();
 $PRODUCT_ID = array();
-
 $res_diff = array();
 
-//сделать автоматический подсчет максимальной длины для for
 
+//пошаговое сравнение
+$i = 0;
+$count = 0;
+$limit = $LIMIT;
 
-
-for($i = 0; $i<=20; $i++){
-	if($arFields = $csvFile->Fetch()) 
-	{	
-		$PROP = array(); 
-		$PROP['name'] = $arFields[1];
-		$PROP['preview_text'] = $arFields[2]; 
-		$PROP['detail_text'] = $arFields[3];
-		$PROP['prop1'] = $arFields[4];  
-		$PROP['prop2'] = $arFields[5];
-		$array_csv[$i] = $PROP;
-
-	};
-	if($ob = $res->GetNextElement())
-	{	
-		$result = array();
-		$arFields = $ob->GetFields();
-		$result['name'] = $arFields['NAME'];
-		$result['preview_text'] = $arFields['PREVIEW_TEXT'];
-		$result['detail_text'] = $arFields['DETAIL_TEXT'];
-		$result['prop1'] = $arFields['PROPERTY_PROP1_VALUE'];
-		$result['prop2'] = $arFields['PROPERTY_PROP2_VALUE'];
-		$ress[$i] = $result;
-		$PRODUCT_ID[$i] = $arFields['ID'];
+while($i!=$STEP){
+	while($count < $limit){
+		if($arFields = $csvFile->Fetch()) 
+		{	
+			$PROP = array(); 
+			$PROP['name'] = $arFields[1];
+			$PROP['preview_text'] = $arFields[2]; 
+			$PROP['detail_text'] = $arFields[3];
+			$PROP['prop1'] = $arFields[4];  
+			$PROP['prop2'] = $arFields[5];
+			$array_csv[$count] = $PROP;
+	
+		};
+			if($ob = $res->GetNextElement())
+		{	
+			$result = array();
+			$arFields = $ob->GetFields();
+			$result['name'] = $arFields['NAME'];
+			$result['preview_text'] = $arFields['PREVIEW_TEXT'];
+			$result['detail_text'] = $arFields['DETAIL_TEXT'];
+			$result['prop1'] = $arFields['PROPERTY_PROP1_VALUE'];
+			$result['prop2'] = $arFields['PROPERTY_PROP2_VALUE'];
+			$ress[$count] = $result;
+			$PRODUCT_ID[$count] = $arFields['ID'];
+		}
+		$count++;
 	}
+	if($count==$limit){
 
-	$res_diff = diff($array_csv[$i],$ress[$i]);
+		$res_diff = diff($array_csv,$ress);
+		if(!empty($res_diff))
+			updateUp($res_diff, $PRODUCT_ID, $BLOCK_ID);
 
-	if(!empty($res_diff)){
-		updateUp($res_diff, $PRODUCT_ID[$i], $BLOCK_ID);
-	} else echo"";
+		$array_csv = array();
+		$ress = array();
+		$limit = $count + $LIMIT;
+		$i++;
+	}
 };
 
 
@@ -74,31 +88,38 @@ for($i = 0; $i<=20; $i++){
 
 // Функция на изменение/добавление элементов
 function updateUp($array_diff,$prod_id, $block_id){
+
+
 	$el = new CIBlockElement;
-	$keys = array_keys($array_diff);
+
 	$arLoadProductArray = Array(
 	  "IBLOCK_ID"      => $block_id,
-	  "ACTIVE"         => "Y",
+	  "ACTIVE"         => "Y",	
 	);
-	$PROP = array();
-	foreach($keys as $k=>$v){
-	if($keys[$k] == 'name') $arLoadProductArray["NAME"] = $array_diff["name"];
-	if($keys[$k] == 'preview_text') $arLoadProductArray["PREVIEW_TEXT"] = $array_diff["preview_text"];
-	if($keys[$k] == 'detail_text') $arLoadProductArray["DETAIL_TEXT"] = $array_diff["detail_text"];
-	if(($keys[$k] == 'prop1') || ($keys[$k] == 'prop2')) {
-		if($keys[$k] == 'prop1')
-		$PROP['prop1'] = $array_diff['prop1'];
-		if($keys[$k] == 'prop2')
-		$PROP['prop2'] = $array_diff['prop2'];
-		CIBlockElement::SetPropertyValuesEx($prod_id, $block_id, $PROP);
-	}
-};
-		if($res = $el->Update($prod_id, $arLoadProductArray)){
-		echo"Я изменил";
+
+
+
+	foreach($array_diff as $k=>$v){
+		$PROP = array();
+		if(in_array($array_diff[$k]['name'],$array_diff[$k])) $arLoadProductArray["NAME"] = $array_diff[$k]["name"];
+		if(in_array($array_diff[$k]['preview_text'],$array_diff[$k])) $arLoadProductArray["PREVIEW_TEXT"] = $array_diff[$k]["preview_text"];
+		if(in_array($array_diff[$k]['detail_text'],$array_diff[$k])) $arLoadProductArray["DETAIL_TEXT"] = $array_diff[$k]["detail_text"];
+		if(in_array($array_diff[$k]['prop1'],$array_diff[$k]) || in_array($array_diff[$k]['prop2'],$array_diff[$k])) {
+			if(in_array($array_diff[$k]['prop1'],$array_diff[$k]))
+			$PROP['prop1'] = $array_diff[$k]['prop1'];
+			if(in_array($array_diff[$k]['prop2'],$array_diff[$k]))
+			$PROP['prop2'] = $array_diff[$k]['prop2'];
+			print_r($PROP);
+			}
+			CIBlockElement::SetPropertyValuesEx($prod_id[$k], $block_id, $PROP);
+
+		if($res = $el->Update($prod_id[$k], $arLoadProductArray)){
+		echo"Я изменил поля";
 		} else {
-			$arLoadProductArray["PROPERTY_VALUES"] = $PROP;
-			$res_add = $el->Add($arLoadProductArray);
-		}
+		$arLoadProductArray["PROPERTY_VALUES"] = $PROP;
+		if($res = $el->Add($arLoadProductArray))echo"я добавил элeмент";}
+
+	};
 };
 
 // Разность двух массивов
